d61862be274496153d51dba6518c2cb624cb8d1c
Krishna <krishna9842740994@gmail.com>
2026-07-31 19:50:58 +0530

    created sep folder for each codes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,18 @@
 const express= require('express');
+const authRoutes=require('./routes/auth.routes');
 
 const app=express();
+
+app.use(express.json());   // to parse incoming JSON requests
+
+
+
+
+// so if i want to the register route i have to go to /api/auth/register then only i can use the resgister route api 
+
+app.use('/api/auth',authRoutes);
+
+
 
 
 
